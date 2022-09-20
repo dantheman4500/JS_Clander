9207180bcbdb42238d48673e6d7ca8663ecfeb77
Daniele <danielromero4500@gmail.com>
2022-09-20 16:07:57 -0700

    pdates dom based off time of day and saves decriptions

--- script.js
+++ script.js
@@ -10,24 +10,21 @@ $(document).ready(function () {
       localStorage.setItem(time, event);
 //----------Handels the modal popup-------------//
         modalEL.show();
-        $("#modalMsg").text(event+ " was marked on your planners!!!")
+        $("#modalMsg").text(event+ "was marked on your planners!!!")
         
     });
 //------------Hides Modals---------------------//
     span.onclick = function() {
         modalEL.hide();
     };
-
-
-
 //--- get the hours --------------//
  
     function getHour(){
-    var currentHour = moment().hours();
+    var currentHour = moment().hour();
 
     $('.timeSlot').each(function () {
         var timeBlocks = parseInt($(this).attr('id').split('-')[1]);
-        console.log(timeBlocks)
+        console.log(currentHour)
 //---Changes the color of the text boxes---//
 
         if (timeBlocks < currentHour) {
@@ -49,4 +46,18 @@ getHour();
 var interval = setInterval(getHour, 15000);
 
 // -----------------------------//
+
+$('#AM-9 .description').val(localStorage.getItem('AM-9'));
+$('#AM-10 .description').val(localStorage.getItem('AM-10'));
+$('#AM-11 .description').val(localStorage.getItem('AM-11'));
+$('#PM-12 .description').val(localStorage.getItem('#PM-12'));
+$('#PM-13 .description').val(localStorage.getItem('PM-13'));
+$('#PM-14 .description').val(localStorage.getItem('PM-14'));
+$('#PM-15 .description').val(localStorage.getItem('PM-15'));
+$('#PM-16 .description').val(localStorage.getItem('PM-16'));
+$('#PM-17 .description').val(localStorage.getItem('PM-17'));
+
+// display current day and time on page
+$('#currentDay').text(moment().format('dddd, MMMM Do',));
+$('#currentTime').text(moment().format('LTS'));
 });
